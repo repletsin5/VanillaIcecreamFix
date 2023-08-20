65f66aa7014c15af3af4c9f566059418d9f0adea
repletsin5 <50048371+repletsin5@users.noreply.github.com>
2023-08-20 18:28:13 +0100

    Revert "Forge mod src commit"

---
+++ src/main/java/me/replet/client/modsupport/ModMenuSupport.java
@@ -0,0 +1,18 @@
+package me.replet.client.modsupport;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import org.dimdev.vanillafix.util.config.ModConfig;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ModMenuSupport implements ModMenuApi {
+	@Override
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+		return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+	}
+}
---
+++ src/main/java/org/dimdev/vanillafix/bugs/BugFixMixinPlugin.java
@@ -0,0 +1,68 @@
+package org.dimdev.vanillafix.bugs;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dimdev.vanillafix.VanillaFix;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+public class BugFixMixinPlugin implements IMixinConfigPlugin {
+	@Override
+	public void onLoad(String mixinPackage) {
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+//		LogManager.getLogger("VanillIcecreamFix").warn(mixinClassName.compareTo("org.dimdev.vanillafix.bugs.mixins.step.ServerPlayerEntityMixin")==0);
+//
+//		if(mixinClassName.compareTo("org.dimdev.vanillafix.bugs.mixins.step.ServerPlayerEntityMixin")==0){
+//			try {
+//				Class<?> clazz = Class.forName("org.quiltmc.loader.impl.game.minecraft.Hooks");
+//
+//				return false;
+//
+//			} catch (ClassNotFoundException e) {
+//				LogManager.getLogger("VanillIcecreamFix").error(e);
+//			}
+//		}
+
+		return Optional.ofNullable(VanillaFix.MIXIN_CONFIGS.get(targetClassName)).map(pair -> {
+			try {
+				Object e = VanillaFix.config()
+						.getClass()
+						.getField(pair.getCategory())
+						.get(VanillaFix.config());
+				return e.getClass().getField(pair.getValue()).getBoolean(e);
+			} catch (IllegalAccessException | NoSuchFieldException ignored) {
+				throw new AssertionError();
+			}
+		}).orElse(Boolean.TRUE);
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+}
